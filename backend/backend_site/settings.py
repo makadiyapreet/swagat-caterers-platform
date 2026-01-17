f4150b1dc472abde9cd37f5171e78a0603f5a148
makadiyapreet <makadiyapreet1@gmail.com>
2026-01-17 20:17:52 +0530

    Change all codes to django static

--- backend/backend_site/settings.py
+++ backend/backend_site/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [
