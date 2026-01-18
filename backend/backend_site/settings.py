8009ecd11bdf6b6d26080a0e0c46d2816707b5ea
makadiyapreet <makadiyapreet1@gmail.com>
2026-01-18 19:56:13 +0530

    Error Correction

--- backend/backend_site/settings.py
+++ backend/backend_site/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # FIX: Look in the sibling 'static' folder for static assets too
 STATICFILES_DIRS = [
-    BASE_DIR / 'templates',
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
