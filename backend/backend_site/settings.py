dc886c2f6a7b00d516c620b04c172646374b6c68
makadiyapreet <makadiyapreet1@gmail.com>
2026-01-18 11:53:11 +0530

    Changes in smtp codes

--- backend/backend_site/settings.py
+++ backend/backend_site/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'corsheaders',
-    'catering',
+    'catering.apps.CateringConfig',
     'cloudinary_storage',
     'cloudinary',
 ]
