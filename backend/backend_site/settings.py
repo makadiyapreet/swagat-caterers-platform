9c422362b58c3896710e48ee4c09859ef9a15eb3
makadiyapreet <makadiyapreet1@gmail.com>
2026-01-18 12:05:52 +0530

    Changes in smtp codes

--- backend/backend_site/settings.py
+++ backend/backend_site/settings.py
@@ -156,9 +156,9 @@ REST_FRAMEWORK = {
 # --- EMAIL SETTINGS (SMTP) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'swagatcaterersofficial@gmail.com'
 EMAIL_HOST_PASSWORD = 'cqib xpwa cxwo mtri'  # App Password
 DEFAULT_FROM_EMAIL = 'swagatcaterersofficial@gmail.com'
